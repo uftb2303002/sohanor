5a6d84c9dee1c60f3b541766388adb280dbe26d2
Sohanor Rahaman <sohanorrahaman2024@gmail.com>
2025-12-17 16:52:09 +0600

    version3

--- oneArraysum.c
+++ oneArraysum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int a[3][4]={{100,200,150,120},{300,250,400,320},{90,80,70,60}};
-    int i,j;
+    int i,j
     int product =0;
     int day=0;
     for(i=0;i<3;i++){
@@ -21,4 +21,5 @@ int main(){
          printf("\nday%d:%d\n",j+1,day);
     }
     return 0;
+
 }
